abad7d11efcb3649f8b1413f6cdf470ec8d92209
Michael Matus <michael@matus.at>
2025-10-11 19:33:16 +0200

    Implement transmission calculation

--- Bev.Instruments.Thorlabs.Ccs/SpecMath.cs
+++ Bev.Instruments.Thorlabs.Ccs/SpecMath.cs
@@ -4,7 +4,6 @@ namespace Bev.Instruments.Thorlabs.Ccs
 {
     public static class SpecMath
     {
-
         public static CalcData Subtract(CalcData signal, CalcData bckgnd)
         {
             double[] wl = new double[signal.Wavelengths.Length];
@@ -15,8 +14,8 @@ namespace Bev.Instruments.Thorlabs.Ccs
             {
                 wl[i] = signal.Wavelengths[i];
                 newSignal[i] = signal.AverageValues[i] - bckgnd.AverageValues[i];
-                newNoise[i] = Math.Sqrt(Math.Pow(signal.NoiseValues[i], 2) + Math.Pow(bckgnd.NoiseValues[i], 2));
-                newStdDev[i] = Math.Sqrt(Math.Pow(signal.StdDevValues[i], 2) + Math.Pow(bckgnd.StdDevValues[i], 2));
+                newNoise[i] = SqSum(signal.NoiseValues[i], bckgnd.NoiseValues[i]);
+                newStdDev[i] = SqSum(signal.StdDevValues[i], bckgnd.StdDevValues[i]);
             }
             return new CalcData(wl,newSignal, newNoise, newStdDev);
         }
@@ -31,11 +30,22 @@ namespace Bev.Instruments.Thorlabs.Ccs
             {
                 wl[i] = signal.Wavelengths[i];
                 newSignal[i] = (signal.AverageValues[i] - bckgnd.AverageValues[i])/(reference.AverageValues[i] - bckgnd.AverageValues[i]);
-                newNoise[i] = Math.Sqrt(Math.Pow(signal.NoiseValues[i], 2) + Math.Pow(bckgnd.NoiseValues[i], 2));
-                newStdDev[i] = Math.Sqrt(Math.Pow(signal.StdDevValues[i], 2) + Math.Pow(bckgnd.StdDevValues[i], 2));
+                newNoise[i] = RelUncXXX(signal.AverageValues[i], reference.AverageValues[i], bckgnd.AverageValues[i], signal.NoiseValues[i], reference.NoiseValues[i], bckgnd.NoiseValues[i]);
+                newStdDev[i] = RelUncXXX(signal.AverageValues[i], reference.AverageValues[i], bckgnd.AverageValues[i], signal.StdDevValues[i], reference.StdDevValues[i], bckgnd.StdDevValues[i]); ;
             }
             return new CalcData(wl, newSignal, newNoise, newStdDev);
         }
 
+        private static double SqSum(double u1, double u2) => Math.Sqrt(u1 * u1 + u2 * u2);
+        
+        private static double RelUncXXX(double x, double xr, double xb, double ux, double uxr, double uxb)
+        {
+            double v1 = 1.0 / (xr - xb);
+            double v2 = v1 * v1;
+            double u1 = v1 * ux;
+            double u2 = v2 * (x - xr) * uxb;
+            double u3 = v2 * (xb - x) * uxr;
+            return Math.Sqrt((u1 * u1) + (u2 * u2) + (u3 * u3) );
+        }
     }
 }
--- CCStest/Program.cs
+++ CCStest/Program.cs
@@ -17,32 +17,71 @@ namespace CCStest
             Console.WriteLine($"User Text:                {tlCcs.InstrumentUserText}");
             Console.WriteLine($"Min Wavelength:           {tlCcs.MinimumWavelength:F2} nm");
             Console.WriteLine($"Max Wavelength:           {tlCcs.MaximumWavelength:F2} nm");
+            Console.WriteLine();
+
+            int nSamples = 20;
+
+            SpectralData dark = new SpectralData(tlCcs.Wavelengths);
+            SpectralData filter = new SpectralData(tlCcs.Wavelengths);
+            SpectralData reference = new SpectralData(tlCcs.Wavelengths);
+
+            tlCcs.SetIntegrationTime(0.37);
+
+
+            //Measure the reference spectrum
             Console.WriteLine(  );
-
-            SpectralData darkSpec = new SpectralData(tlCcs.Wavelengths);
-
-            tlCcs.SetIntegrationTime(50);
-            darkSpec.UpdateSignal(tlCcs.GetSingleScanData());
-            Console.WriteLine($"{tlCcs.GetIntegrationTime():F5} {darkSpec.MaximumSignal:F5}");
-
-            int[] adCounts = tlCcs.GetSingleRawScanData();
-
-            for (int i = 0; i < adCounts.Length; i += 1)
+            Console.WriteLine("Press <ENTER> to start measurement of reference spectrum.");
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
             {
-                Console.WriteLine($"{i,5}: {adCounts[i]} AD counts");
+                for (int i = 0; i < nSamples; i++)
+                {
+                    reference.UpdateSignal(tlCcs.GetSingleScanData());
+                    Console.Write(".");
+                }
+            }
+            Console.WriteLine(  );
+            Console.WriteLine("Press <ENTER> to start measurement of sample spectrum.");
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            { 
+                for (int i = 0; i < nSamples; i++)
+                {
+                    filter.UpdateSignal(tlCcs.GetSingleScanData());
+                    Console.Write(".");
+                }
+        }
+            Console.WriteLine(  );
+            Console.WriteLine("Press <ENTER> to start measurement of dark spectrum.");
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                for (int i = 0; i < nSamples; i++)
+                {
+                    dark.UpdateSignal(tlCcs.GetSingleScanData());
+                    Console.Write(".");
+                }
             }
 
+            var refCalc = new CalcData(reference);
+            var darkCalc = new CalcData(dark);  
+            var filterCalc = new CalcData(filter);  
 
-            //double integrationTime = 0.00001; // seconds
-            //while (integrationTime < 100)
-            //{
-            //    darkSpec.Clear();
-            //    tlCcs.SetIntegrationTime(integrationTime);
-            //    darkSpec.UpdateSignal(tlCcs.GetSingleScanData());
-            //    Console.WriteLine($"{tlCcs.GetIntegrationTime():F5} {darkSpec.MaximumSignal:F5}");
-            //    if (darkSpec.IsOverexposed) break;
-            //    integrationTime *= 1.1;
-            //}
+            var transmission = SpecMath.RelXXX(filterCalc,refCalc,darkCalc);
+
+            Console.WriteLine("Wavelength (nm)  Transmission  Noise");  
+            for (int i = 0; i < transmission.Wavelengths.Length; i++)
+            {
+                Console.WriteLine($"{transmission.Wavelengths[i],12:F2} {transmission.AverageValues[i],8:F5} {transmission.NoiseValues[i],8:F5}");
+            }
+
+        //double integrationTime = 0.00001; // seconds
+        //while (integrationTime < 100)
+        //{
+        //    darkSpec.Clear();
+        //    tlCcs.SetIntegrationTime(integrationTime);
+        //    darkSpec.UpdateSignal(tlCcs.GetSingleScanData());
+        //    Console.WriteLine($"{tlCcs.GetIntegrationTime():F5} {darkSpec.MaximumSignal:F5}");
+        //    if (darkSpec.IsOverexposed) break;
+        //    integrationTime *= 1.1;
+        //}
 
         }
     }
